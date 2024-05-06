c01979d3d7ff4fc227f88b4d6601b07148a48af2
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-05-06 10:41:02 +0200

    Adding new Datei Monday work

---
+++ src/hooks/api.tsx
@@ -0,0 +1,15 @@
+import useFetch from "react-fetch-hook";
+import { Note } from "../types/notes.type";
+
+export const useGetNotes = () => {
+    const { isLoading, data, error } = useFetch<Note[]>("http://localhost:3000/notes", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Galina'
+        }
+    });
+
+    return { isLoading, data, error }
+}
+
